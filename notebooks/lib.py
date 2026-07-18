7b6efd92c055cd3130b61c140b3cec871186637c
Nathan Edwards <edwardsnj@gmail.com>
2026-07-18 10:05:54 -0400

    tweak TF-IDF to give features for all accessions

--- notebooks/lib.py
+++ notebooks/lib.py
@@ -4,7 +4,7 @@ GITHUB = "https://raw.githubusercontent.com/EdwardsLabProjects/pride-study-retri
 import os, os.path, subprocess
 import pandas
 
-VERSION='1.0.4'
+VERSION='1.0.5'
 
 def download_embeddings(model="openai-3-small"):
     # files...
@@ -99,7 +99,7 @@ def split_train_test(allacc, seeds, neg_seeds, test_size=0.2, bgsize=25):
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-def create_tfidf_features(md_dataframe, train_accessions, train_y, test_accessions, positive_only=False,**kwargs):
+def create_tfidf_features(md_dataframe, train_accessions, train_y, all_accessions, positive_only=False,**kwargs):
     # Filter training accessions to include only 'true cases' (where train_y is 1)
     if positive_only:
       true_train_accessions = [acc for acc, y_val in zip(train_accessions, train_y) if y_val == 1]
@@ -132,7 +132,7 @@ def create_tfidf_features(md_dataframe, train_accessions, train_y, test_accessio
     )
 
     # Prepare testing data for TF-IDF transformation
-    md_test = md_dataframe[md_dataframe['prideacc'].isin(test_accessions)].set_index('prideacc').loc[test_accessions]
+    md_test = md_dataframe[md_dataframe['prideacc'].isin(all_accessions)].set_index('prideacc').loc[list(all_accessions)]
     test_texts = md_test['text']
 
     # Apply the fitted TF-IDF model to test data (transform only)
